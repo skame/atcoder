4ea83f06d28c8fa34a50405cd98803972e708293
skame <s@kamei.nu>
2020-05-10 10:05:44 +0900

    AC

---
+++ abc081/B/main.py
@@ -0,0 +1,13 @@
+import sys
+
+n = input()
+l = list(map(int, input().split()))
+
+loop = 0
+
+while (1):
+  for i in l:
+    if i % (2 ** (loop + 1)) != 0:
+      print(loop)
+      sys.exit(0)
+  loop = loop + 1
